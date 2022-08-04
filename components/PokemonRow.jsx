dca7c1a66a248aa4568f141b7b5a9c2f5845e57a
RoryJonesdB <rory.jones@dbbroadcast.co.uk>
2022-08-04 10:55:29 +0100

    hydration errors but the rest of it seems to work

---
+++ components/PokemonRow.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link"
+import { Button } from "@mui/material";
+
+
+import PokemonType from "../src/pokemonType";
+
+
+const PokemonRow = ({ pokemon, onClick }) => (
+  <>
+    <tr >
+      <td>
+        <Link href={`/pokemon/${pokemon.id}`}>
+          <a>{pokemon.name.english}</a>
+        </Link>
+      </td>
+      <td>{pokemon.type.join(", ")}</td>
+      <td>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => onClick(pokemon)}
+        >
+          More Information
+        </Button>
+      </td>
+    </tr>
+  </>
+);
+
+PokemonRow.propTypes = {
+  pokemon: PokemonType,
+};
+
+export default PokemonRow
